ffa749b8267358b8849aca68293cdd3b3a30e45b
Lamboprogramming <gillespielamar@gmail.com>
2022-03-31 23:53:20 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine
             height="100vh"
-            projectID="cde81a12-a3d2-4a80-ab43-ac39c9a5e5e2"
-            userName="chatadmin"
-            userSecret="password123"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+            projectID='cde81a12-a3d2-4a80-ab43-ac39c9a5e5e2'
+            userName='chatadmin'
+            userSecret='password123'
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
             />
     );
-}
+};
    
-    
  export default App;
